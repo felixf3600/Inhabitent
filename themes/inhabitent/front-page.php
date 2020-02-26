976c581bf6cb1e0e576ffaff17c5000fecdd86b0
felixf3600 <felixf3600@gmail.com>
2020-02-26 13:46:20 -0800

    added custom loop for journal in front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -6,32 +6,48 @@
  */
 
 get_header(); ?>
-<!--?php // check if the post or page has a Featured Image assigned to it.
-if ( has_post_thumbnail() ) {
-    /* grab the url for the full size featured image */
-	$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
-	// adds thumbnail url to class
-	echo "<style> 
-	.site-header {
-	background-image: url($featured_img_url);
-	width: 100vw;
-	height: 100vh;
-	};
-	</style>";
-}?-->
 <div class="banner-image">
-<?php
-	the_post_thumbnail('large');
-?>
+
+<?php // check if the pafe is about or front page
+if (is_page('page-about')|| is_front_page()) { 
+	if ( has_post_thumbnail() ) {
+	// adds thumbnail url to class
+	the_post_thumbnail('full');
+}}
+?>	
+<div class = "inner-logo">
 </div>
+</div>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<?php
+			   $args = array( 
+				 'numberposts' => '3'
+			);
+   			$product_posts = get_posts( $args ); // returns an array of posts
+			?>
+ 
+			<h2>INHABITENT JOURNAL</h2>;
+			<div class="loop-containers">
+			<?php 
+			foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+				<div class= "single-container">
+					<?php /* Content from your array of post results goes here */ ?>
+					<?php the_post_thumbnail();the_date();get_default_comment_status(); ?>
+					<h3> <?php the_title(); ?> </h3>
+					<a href= "<?php the_permalink(); ?>">				
+					<botton	type="button">Read Entry</button>
+					</a>
+				</div>
+			<?php endforeach; wp_reset_postdata(); ?>
+			</div>
+		
+			
+			<?php// while ( have_posts() ) : the_post(); ?>
+				<?php// get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+			<?php// endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
---
+++ themes/inhabitent/page-about.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+<div class="banner-image">
+<?php
+	the_post_thumbnail('full');
+?>
+</div>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
